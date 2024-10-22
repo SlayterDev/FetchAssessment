ddc37b9480ea22ed5a7a38285cb66ba424aa2296
Brad Slayter <SlayterDev@users.noreply.github.com>
2024-10-22 07:55:43 -0500

    Initial Commit

---
+++ FetchRecipe/FetchRecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchRecipeApp.swift
+//  FetchRecipe
+//
+//  Created by Brad Slayter on 10/22/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchRecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FetchRecipeTests/FetchRecipeTests.swift
@@ -0,0 +1,17 @@
+//
+//  FetchRecipeTests.swift
+//  FetchRecipeTests
+//
+//  Created by Brad Slayter on 10/22/24.
+//
+
+import Testing
+@testable import FetchRecipe
+
+struct FetchRecipeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
